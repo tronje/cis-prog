591fefe3b3f799c1f7e13395335ab73d334e364e
Quintol <jorim.kornblueh@t-online.de>
2015-04-06 15:05:35 +0200

    fixed wrong formatings that lead to -1 output(Overflowgit add minmax.c)

--- Blatt01-HeidmannKornbluehKrabbe/Aufgabe3/minmax.c
+++ Blatt01-HeidmannKornbluehKrabbe/Aufgabe3/minmax.c
@@ -15,14 +15,14 @@ int main(void) {
     printf("char\t%d\t%d\n", SCHAR_MIN, SCHAR_MAX);
     printf("short\t%d\t%d\n", SHRT_MIN, SHRT_MAX);
     printf("int\t%d\t%d\n", INT_MIN, INT_MAX);
-    printf("long\t%d\t%d\n", LONG_MIN, LONG_MAX);
+    printf("long\t%ld\t%ld\n", LONG_MIN, LONG_MAX);
 
     printf("\n== unsigned Variables ==\n");
     printf("name\tmin\tmax\n");
     printf("char\t%d\t%d\n", 0, CHAR_MAX);
     printf("short\t%d\t%d\n", 0, USHRT_MAX);
-    printf("int\t%d\t%d\n", 0, UINT_MAX);
-    printf("long\t%d\t%d\n", 0, ULONG_MAX);
+    printf("int\t%d\t%u\n", 0, UINT_MAX);
+    printf("long\t%d\t%lu\n", 0, ULONG_MAX);
 
 
 
