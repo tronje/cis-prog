83bf75d25793729aecd83aacde63d2ebaa0dd994
tronje <tronjekrabbe@gmail.com>
2015-04-09 14:21:12 +0200

    fix indentation for 01/Aufgabe4

--- Blatt01-HeidmannKornbluehKrabbe/Aufgabe4/leapyear.c
+++ Blatt01-HeidmannKornbluehKrabbe/Aufgabe4/leapyear.c
@@ -2,31 +2,31 @@
 
 void usage(void)
 {
-  fprintf(stderr, "Usage: leapyear.x yearToTest\n");
+    fprintf(stderr, "Usage: leapyear.x yearToTest\n");
 }
 
 int main(int argc, char *argv[])
 {
-  int year;
-  if (argc == 2)
+    int year;
+    if (argc == 2)
     {
-      if (sscanf(argv[1], "%d", &year))
-	{
-	  if(!(year % 4) && ((year % 100) || !(year % 400)))
-	    printf("The year %d is an leapyear.\n", year);
-	  else
-	    printf("The year %d is no leapyear.\n", year);
-	}      
-      else
-	{
-	  fprintf(stderr, "invalid input\n");
-	  usage();
-	}    
+        if (sscanf(argv[1], "%d", &year))
+        {
+            if(!(year % 4) && ((year % 100) || !(year % 400)))
+                printf("The year %d is an leapyear.\n", year);
+            else
+                printf("The year %d is no leapyear.\n", year);
+        }      
+        else
+        {
+            fprintf(stderr, "invalid input\n");
+            usage();
+        }    
     }
-  else
+    else
     {
-      fprintf(stderr, "wrong number of arguments\n");
-      usage();
+        fprintf(stderr, "wrong number of arguments\n");
+        usage();
     }
-  return 0;
+    return 0;
 }
