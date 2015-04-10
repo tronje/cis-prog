0457936bcf73c434dee3fe259d98c2eee7a43fde
NodmGatall <oliverheidmann@hotmail.de>
2015-04-10 20:53:04 +0200

    changed binarySearchSmallerK.c to binarySearchsmallerK.h / added subsetsum.c and implementet a first version of subsetsum() which doesnt work completly

---
+++ Blatt02-HeidmannKornbluehKrabbe/Aufgabe1/binarySearchSmallerK.h
@@ -0,0 +1,86 @@
+#ifndef BINARYSEARCHSMALLERK_H
+#define BINARYSEARCHSMALLERK_H
+#include <stdbool.h>
+#include <stdio.h>
+/* 
+	Binary search to find the next smaller entry after <k>. returns exit_success if succsesfull and exit_failure if 
+*/
+bool binarySearchFirstSmallerK(const unsigned long *arr, unsigned long k,unsigned long n, unsigned long* index)
+{
+	if(arr[0] >= k) return false;
+
+	unsigned long currentIndex = n/2;
+	unsigned long size = n;
+	while(size > 1)
+	{
+		//printf("Current: %d\n", currentIndex);
+		//printf("size: %d\n", size);
+		size /= 2;
+		if(size % 2 != 0 && size != 1){
+			size++;
+			currentIndex++;	
+		}
+		if(arr[currentIndex] == k)
+		{
+			*index = currentIndex -1;
+			return true;
+			//puts("If1\n");
+		}
+		if(arr[currentIndex] > k)
+		{
+			currentIndex -= size ;
+			//puts("IF2\n");	
+		}
+		else//(arr[currentIndex] < k)
+		{
+			currentIndex += size;
+			//puts("IF3\n");	
+		}
+		//printf("%d\n",size );	
+	}
+	*index = currentIndex - 1;
+	return true;
+
+
+// int main()
+// {
+// 	unsigned long arr[50];
+// 	int lol = 0;
+// 	int lmao = 0;
+// 	int n = 50;
+// 	for(; lol< n; lol++)
+// 	{
+// 		if(lol == 0)
+// 		{
+// 			lol++;
+// 		}
+// 		if(lol == 21)
+// 		{
+// 			lol++;
+// 		}
+// 		if(lol == 22)
+// 		{
+// 			lol++;
+// 		}
+// 		arr[lmao] = lol;
+// 		lmao++;
+		
+// 	}
+// 	unsigned long index = 0;
+// 	unsigned long testVar = 0;
+// 	for(; testVar < n; testVar++)
+// 	{
+// 		printf("%lu\n", index);
+// 		if(binarySearchFirstSmallerK(arr,testVar,n,&index)){
+// 		printf("RESULT: testVar: %lu value:%lu\n",testVar, arr[index]);
+// 	}
+// 	else
+// 	{
+// 		//printf("LOL");
+// 		puts("BIN NO FIND ANy\n");
+// 	}
+// 	}
+// 	return 0;
+
+}
+#endif
--- Blatt02-HeidmannKornbluehKrabbe/Aufgabe1/subsetsum.h
+++ Blatt02-HeidmannKornbluehKrabbe/Aufgabe1/subsetsum.h
@@ -9,7 +9,6 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
-#include "binarySearchSmallerK.c"
 /* Returns true if the ordered set of numbers <arr> of size <n> contains a
    subset with the sum of its members equal to <k>, returns false if no such
    subset exists. Elements in <mark> will be set to true for all members of
@@ -23,42 +22,6 @@
 bool subsetsum(unsigned long k,
         bool *mark,
         const unsigned long *arr,
-        unsigned long n)
-{
-    unsigned long rest = k;
-    unsigned long nextSmallerKIndex = binarySearchFirstSmallerK(arr,rest,n);
+        unsigned long n);
 
-    if (k == 0) {
-        return false;
-    }
-
-    if(arr[nextSmallerKIndex+1] == k)
-    {
-        mark[nextSmallerKIndex+1] = 1;
-        return 1;
-    }
-    while(rest != 0 && nextSmallerKIndex != 0)
-    {
-        nextSmallerKIndex = binarySearchFirstSmallerK(arr,rest,n);
-        rest -= arr[nextSmallerKIndex];
-        mark[nextSmallerKIndex] = 1;
-        if(rest > k)
-        {
-            rest = k;
-            memset(mark,0,n * sizeof(mark[0]));
-        }
-    }
-
-    mark = 0;
-    printf("Smaller %lu\n",nextSmallerKIndex);
-    printf("rest %lu\n", rest);
-    // nehme naechsten kleineren zu k.
-    // k - ersten = rest
-    // gucken ob irgend eine zahl im forderen rest des arrays mit ersten zusammen 0 ergibt
-    // wenn nicht den gucke welcher naechste einen am naehesten an das ergebniss bringt
-    // dann zeile 8
-
-    // sollte das alles nicht funktionieren -> gehe auf den 2. niedrigsten und wiederhole den ganzen shit
-    return true;
-}
-
+#endif
