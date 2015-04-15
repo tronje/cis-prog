6e39c71e2b041ec75939ae6ee0221b74d4ccccd2
tronje <tronjekrabbe@gmail.com>
2015-04-15 09:11:01 +0200

    add some comments to subsetsum.c. Still not working, no idea why

--- Blatt02-HeidmannKornbluehKrabbe/Aufgabe1/subsetsum.c
+++ Blatt02-HeidmannKornbluehKrabbe/Aufgabe1/subsetsum.c
@@ -11,25 +11,37 @@ bool subsetsum(unsigned long k,
     unsigned long startIndex;
     unsigned long index;
     unsigned long rest = k;
+
+    // this is clumsy, but ultimately doesn't hurt.
+    // we're not checking for this elsewhere...
+    if(arr[0] == k) {
+        return true;
+    }
+
+    // if we can't find an element in arr that is smaller
+    // than k, there exists no valid subset
     if(!binarySearchFirstSmallerK(arr,k,n,&startIndex))
     {
         return false;
     }
+
     if(k == arr[startIndex + 1])
     {
         mark[startIndex + 1] = 1;
         return true;
     }
-    else    	
+    else // the non-trivial part of the calculation
     {
         index = startIndex;
         while(rest != 0)
         {
             if(!binarySearchFirstSmallerK(arr,rest+1,n,&index))
             {
+                // reset mark
                 memset(mark,0,sizeof(mark[0]) * n);
                 if(startIndex == 0)
                 {
+                    // we've run out of indeces
                     return false;
                 }
 
