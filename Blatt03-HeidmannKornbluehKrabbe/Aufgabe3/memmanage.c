5b8e44d62826dc858ced402708d9da648a76926d
tronje <tronjekrabbe@gmail.com>
2015-04-21 23:04:20 +0200

    03/Aufgabe3 almost done, not tested yet

--- Blatt03-HeidmannKornbluehKrabbe/Aufgabe3/memmanage.c
+++ Blatt03-HeidmannKornbluehKrabbe/Aufgabe3/memmanage.c
@@ -28,7 +28,7 @@ void enlarge_table(MMspacetable *st) {
 
     // we'll just double the table's size. Why not?
     st->blocks = (MMspaceblock*) realloc(st->blocks,
-                                st->number * 2 * sizeof(MMspaceblock));
+            st->number * 2 * sizeof(MMspaceblock));
     st->number = st->number * 2;
 
     // initialize new entries
@@ -39,9 +39,9 @@ void enlarge_table(MMspacetable *st) {
     }
 }
 
-unsigned long get_block_index(MMspacetable *st, char *file, unsigned long line,
+unsigned long get_index(MMspacetable *st, char *file, unsigned long line,
         void *ptr) {
-    
+
     unsigned long i;
 
     for (i = 0; i < st->number; i++) {
@@ -49,9 +49,11 @@ unsigned long get_block_index(MMspacetable *st, char *file, unsigned long line,
             return i;
     }
 
-    if (ptr != NULL)
+    if (ptr != NULL) {
+        fprintf(stderr, "Invalid pointer! %s line %lu\n", file, line);
         exit(EXIT_FAILURE);
-    
+    }
+
     // the block we were looking for isn't in the table
     // so we need to enlarge it
     enlarge_table(st);
@@ -85,25 +87,63 @@ MMspacetable* mem_man_new(unsigned long numberofblocks) {
 void *mem_man_alloc(MMspacetable *st, char *file, unsigned long line,
         void *ptr, unsigned long size, unsigned long number) {
 
-    st->number += 1;
-    st->blocks = realloc(ptr, (st->));
-    st->blocks->block = realloc(ptr, (number * size));
-    st->blocks->file = file;
-    st->blocks->line = line;
-    st->blocks->size = size;
-    st->blocks->number = number;
+    // get the index at which to alloc stuff,
+    // using our handy-dandy function
+    unsigned long i = get_index(st, file, line, ptr);
 
-    return &(st->blocks->block);
+    // allocate accordingly
+    st->blocks[i].block = realloc(ptr, size * number);
+
+    // set all the attributes
+    st->blocks[i].number = number;
+    st->blocks[i].size = size;
+    st->blocks[i].file = file;
+    st->blocks[i].line = line;
+
+    return st->blocks[i].block;
 }
 
 void mem_man_delete_ptr(MMspacetable *st, char *file, unsigned long line,
         void *ptr) {
-    
-    int i;
-    for (i = 0; i < st->size; i++) {
-        if (st->blocks[i]-)
+
+    unsigned long i = get_index(st, file, line, ptr);
+    st->blocks[i].block = NULL;
+    free(st->blocks[i].block);
+    free(ptr);
+}
+
+void mem_man_info(const MMspacetable *st) {
+    unsigned long i;
+
+    for(i = 0; i < st->number; i++)
+    {
+        if(st->blocks[i].block != NULL)
+        {
+            printf("# active block %lu: allocated in file \"%s\", line %lu\n",
+                    i, st->blocks[i].file, st->blocks[i].line);
+        }
     }
 }
-void mem_man_info(const MMspacetable *st);
-void mem_man_check(const MMspacetable *st);
-void mem_man_delete(MMspacetable *st);
+
+void mem_man_check(const MMspacetable *st) {
+    unsigned long i;
+
+    for(i = 0; i < st->number; i++)
+    {
+        if(st->blocks[i].block != NULL)
+        {
+            fprintf(stderr, "space leak: main memory for block %lu not freed\n"
+                    "%lu cells of size %lu\n"
+                    "allocated: file \"%s\", line %lu\n",
+                    i,
+                    st->blocks[i].number,
+                    st->blocks[i].size,
+                    st->blocks[i].file,
+                    st->blocks[i].line);
+        }
+    }
+}
+void mem_man_delete(MMspacetable *st) {
+    free(st->blocks);
+    free(st);
+}
