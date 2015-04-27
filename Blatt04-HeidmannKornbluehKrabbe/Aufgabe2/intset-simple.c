8ec81f13b21658f540f9c3024a19a307c2d07f68
tronje <tronjekrabbe@gmail.com>
2015-04-27 20:39:56 +0200

    fix a bunch of bugs in intset-simple.c, which is working fine now

--- Blatt04-HeidmannKornbluehKrabbe/Aufgabe2/intset-simple.c
+++ Blatt04-HeidmannKornbluehKrabbe/Aufgabe2/intset-simple.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <limits.h>
 #include "intset.h"
 
 struct IntSet {
     unsigned long maxvalue;
     unsigned long nofelements;
+    unsigned long capacity;
     unsigned long *elements;
 };
 
@@ -25,56 +27,60 @@ IntSet *intset_new(unsigned long maxvalue, unsigned long nofelements) {
 
     // set our IntSet's attributes
     ret->maxvalue = maxvalue;
-    ret->nofelements = nofelements;
+    ret->capacity = nofelements;
+    ret->nofelements = 0;
 
     return ret;
 }
 
 size_t intset_size(unsigned long maxvalue, unsigned long nofelements) {
-    // nofelements+2, because an IntSet contains two unsigned longs in
+    // nofelements+3, because an IntSet contains three unsigned longs in
     // addition to the array of unsigned longs
-    return ((nofelements+2) * sizeof(unsigned long));
+    return ((nofelements+3) * sizeof(unsigned long));
 }
 
 void intset_delete(IntSet *intset) {
+    assert(intset != NULL);
     free(intset->elements);
     free(intset);
 }
 
 void intset_add(IntSet *intset, unsigned long elem) {
-    unsigned long i;
-    i = intset->nofelements;
 
     // make sure elem doesn't exceed the maximum allowed value
-    assert(elem < intset->maxvalue);
+    assert(elem <= intset->maxvalue);
 
     // make sure elem isn't already in our intset
     assert(!intset_is_member(intset, elem));
 
-    // realloc the elements to accomodate for the new member
-    intset->elements = realloc(intset->elements,
-            ( (i + 1) * sizeof(unsigned long)));
-    assert(intset->elements != NULL);
+    // make sure we have enough space
+    assert(intset->nofelements < intset->capacity);
+
+    // make sure the element is larger than any already
+    // included element, as specified in the instructions
+    if (intset->nofelements > 0)
+        assert(elem > intset->elements[intset->nofelements - 1]);
+
+//    if (intset->nofelements + 1 > intset->capacity) {
+//        // realloc the elements to accomodate for the new member
+//        intset->elements = realloc(intset->elements,
+//                ( (intset->capacity * 2) * sizeof(unsigned long)));
+//        assert(intset->elements != NULL);
+//        intset->capacity *= 2;
+//    }
 
     // write the new element to the highest index
-    // note that index i would have been invalid (as indeces start at 0)
-    // but we realloced our array, so now i is the max index, and i+1
-    // is the number of members
-    intset->elements[i] = elem;
-
-    // adjust variable holding the number of elements in the intset
-    intset->nofelements++;
+    intset->elements[intset->nofelements++] = elem;
 }
 
 bool intset_is_member(const IntSet *intset, unsigned long elem) {
-    if (elem > intset->maxvalue)
+    unsigned long first, middle, last;
+
+    if (intset->nofelements == 0)
         return false;
 
-    // for efficiency's sake, we've declared our variables here,
-    // and not at the top of the function, because we can save
-    // the time should the if-clause catch something!
-    // we hope to be excused!
-    unsigned long first, middle, last;
+    if (elem > intset->maxvalue)
+        return false;
 
     first = 0;
     last = intset->nofelements - 1;
@@ -83,14 +89,23 @@ bool intset_is_member(const IntSet *intset, unsigned long elem) {
     // simple little binary search
     while (first <= last) {
         if (intset->elements[middle] < elem) {
+            if (middle == ULONG_MAX)
+                return false;
             first = middle + 1;
         } else if (intset->elements[middle] == elem) {
             return true;
         } else {
+            if (middle == 0)
+                return false;
             last = middle - 1;
         }
-        middle = (first + last)/2;
+        middle = (first + last) / 2;
     }
+//    int i;
+//    for (i = 0; i < intset->nofelements; i++) {
+//        if (intset->elements[i] == elem)
+//            return true;
+//    }
 
     return false;
 }
