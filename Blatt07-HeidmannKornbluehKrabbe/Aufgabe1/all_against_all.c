04f8e7ec366851ee9b6137e31ca8e22b30891454
NodmGatall <oliverheidmann@hotmail.de>
2015-06-01 20:03:47 +0200

    inpmlemented multiseq, read from file, and processing file content into multiseq

---
+++ Blatt07-HeidmannKornbluehKrabbe/Aufgabe1/all_against_all.c
@@ -0,0 +1,64 @@
+#include "multiseq.h"
+#include "string.h"
+#include "unit_edist.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	Multiseq* multiseq;
+	char* file_content;
+	unsigned long file_size;
+  unsigned char* sequence
+  unsigned char* info
+  unsigned long size
+	FILE* pfile;
+
+	pfile = fopen("sw175.fna","r");
+	fseek(pfile,0L,SEEK_END);
+	file_size = ftell(pfile);
+	rewind(pfile);
+
+	file_content = malloc(sizeof(char) * file_size + 1);
+	if(file_content == NULL) return EXIT_FAILURE;
+	fread(file_content,sizeof(char*),file_size,pfile);
+
+	info = (unsigned char*)strtok(file_content,">\n");
+  sequence = (unsigned char*)strtok(NULL,">\n");
+  size = strlen((char*)sequence);
+  multiseq =  gen_new_multiseq(175);
+  while (info != NULL)
+	{
+ 		multiseq_add_sequence(multiseq,sequence,info,size);
+   	info = (unsigned char*)strtok (NULL,">\n");   				
+  	if(info != NULL)
+		{
+    	// puts("=========================");
+			// printf("%s\n",info );
+  		sequence = (unsigned char*)strtok(NULL,">\n");
+   		// printf("%s\n",sequence );
+  		size = strlen((char*)sequence);
+  		// printf("%lu\n", size);
+  	}
+
+  		
+  }
+  // puts("=========================");
+	// puts("=========================");
+ 	puts("ANOTHER TEST");
+ 	printf("%s\n",multiseq -> sequence_infos[3] );
+ 	printf("%s\n",multiseq -> sequences[3] );
+ 	printf("%ld\n",multiseq -> sizes_of_sequence[3] );
+	//multiseq = multiseq_new();
+  printf("%s\n",multiseq -> sequence_infos[2] );
+  printf("%s\n",multiseq -> sequences[2] );
+  unsigned long result = 0;
+	eval_unit_edist(
+	  multiseq -> sequences[3],
+		multiseq -> sizes_of_sequence[3],
+		multiseq -> sequences[2],
+		multiseq -> sizes_of_sequence[2]
+  );
+	printf("==========makeresult: %lu\n",result );
+	return EXIT_SUCCESS;
+}
---
+++ Blatt07-HeidmannKornbluehKrabbe/Aufgabe1/multiseq.h
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Multiseq Multiseq;
+struct Multiseq
+{
+	unsigned char** sequence_infos;
+	unsigned char** sequences;
+	unsigned long* sizes_of_sequence;
+	unsigned int size;
+	unsigned int next_free_space;
+	unsigned int current_entrys;
+
+};
+
+Multiseq* gen_new_multiseq(unsigned int size)
+{
+	Multiseq* new_multiseq = malloc(size * (sizeof(char**) + sizeof(char**) + sizeof(unsigned int*)));
+	if(new_multiseq == NULL) exit( EXIT_FAILURE );
+	new_multiseq -> sequences = malloc(sizeof(char*) * size);
+	if(new_multiseq -> sequences == NULL) exit( EXIT_FAILURE );
+	new_multiseq -> sequence_infos = malloc(sizeof(char*) * size);
+	if(new_multiseq -> sequence_infos== NULL) exit( EXIT_FAILURE );
+	new_multiseq -> sizes_of_sequence =  malloc(sizeof(unsigned int) * size);
+		if(new_multiseq -> sizes_of_sequence == NULL) exit( EXIT_FAILURE );
+	new_multiseq -> size = size;
+	new_multiseq -> next_free_space = 0;
+	new_multiseq -> current_entrys = 0;
+
+	return new_multiseq;
+}
+
+void resize_multiseq(Multiseq* seq)
+{
+	seq -> sequences = realloc(seq -> sequences, seq -> size * 2 * sizeof(char**));
+	seq -> sequence_infos = realloc(seq -> sequence_infos, seq -> size * 2 * sizeof(char**));
+	seq -> sizes_of_sequence = realloc(seq -> sizes_of_sequence,seq -> size * 2 * sizeof(unsigned int));
+}
+
+void multiseq_add_sequence(Multiseq* seq,unsigned char* sequence,unsigned char* sequence_info,unsigned long size)
+{
+	seq -> sequences[seq -> next_free_space] = sequence;
+	seq -> sequence_infos[seq -> next_free_space] = sequence_info;
+	seq -> sizes_of_sequence[seq -> next_free_space] = size;
+	seq -> next_free_space++;
+	seq -> current_entrys++;
+}
