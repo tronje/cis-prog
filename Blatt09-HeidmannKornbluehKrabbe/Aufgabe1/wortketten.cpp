70a966b380fd9f8370fcd5fcc4e8cef05f9194e8
tronje <tronjekrabbe@gmail.com>
2015-06-16 16:55:44 +0200

    start 09/2

---
+++ Blatt09-HeidmannKornbluehKrabbe/Aufgabe1/wortketten.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+typedef vector<string> Wordlist;
+typedef map<string,Wordlist> Adjacencemap;
+
+void read_words() {
+    /* read words from file and save in
+       a vector<string> */
+}
+
+void construct_adjacencemap() {
+    /* construct the adjacencemap */
+}
