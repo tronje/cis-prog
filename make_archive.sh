bdb758457d2d7234e262da38443c1ed6947ae9e5
tronje <tronjekrabbe@gmail.com>
2015-06-17 22:02:58 +0200

    made archive script a little more verbose

--- make_archive.sh
+++ make_archive.sh
@@ -7,6 +7,13 @@ fi
 
 name=Blatt$1-HeidmannKornbluehKrabbe
 
+echo "Creating tar ball..."
 tar -cf $name.tar $name --exclude=*.pdf
+
+echo "Gzipping tar ball..."
 gzip -9 -f $name.tar
+
 echo "Created $name.tar.gz"
+echo "Done!"
+
+exit 0
