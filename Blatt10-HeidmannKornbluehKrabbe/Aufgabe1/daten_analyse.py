977d8b5fbe8488ad564dfa5168324e2ad8956a3a
tronje <tronjekrabbe@gmail.com>
2015-06-18 19:59:20 +0200

    init 10

---
+++ Blatt10-HeidmannKornbluehKrabbe/Aufgabe1/daten_analyse.py
@@ -0,0 +1,22 @@
+'''
+Heidmann
+Kornblueh
+Krabbe
+'''
+
+class daten_analyse:
+
+    T = {'mean': 0, 'std': 0, 'range': 0}
+    v = {'mean': 0, 'std': 0, 'range': 0}
+    n = {'mean': 0, 'std': 0, 'range': 0}
+    w = {'mean': 0, 'std': 0, 'range': 0}
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __str__(self):
+        pass
+
+    def plot_results(self):
+        pass
+
