ed4031979fc059647a6c1972aa283f568dca1fe5
tronje <tronjekrabbe@gmail.com>
2015-06-19 01:18:19 +0200

    more tinkering

--- Blatt10-HeidmannKornbluehKrabbe/Aufgabe1/analyze.py
+++ Blatt10-HeidmannKornbluehKrabbe/Aufgabe1/analyze.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python3
 
 import sys
-from daten_analyse import daten_analyse
+from daten_analyse import daten_analyse as da
 
+filename = input("Bitte geben Sie den Namen der zu untersuchenden Datei: ")
+
+print("Analysiere", filename, "...")
+analyzer = da(filename)
+
+print(analyzer)
+
+print("Konstruiere Graphen...")
+analyzer.plot_results()
+print("Graphen konstruiert in Abb1.pdf und Abb2.pdf!")
--- Blatt10-HeidmannKornbluehKrabbe/Aufgabe1/daten_analyse.py
+++ Blatt10-HeidmannKornbluehKrabbe/Aufgabe1/daten_analyse.py
@@ -12,42 +12,44 @@ class daten_analyse:
         from re import findall
         from numpy import mean, std
 
-        temperatures = []
-        n_of_tests = []
-        velocities = []
-        measurements = []
-        n_of_runs = -1
+        self.filename = filename
+
+        self.temperatures = []
+        self.n_of_tests = []
+        self.velocities = []
+        self.measurements = []
+        self.n_of_runs = -1
 
         with open(filename, 'r') as data_file:
             for line in data_file:
                 if 'Temperatur:' in line:
-                    n_of_runs += 1
-                    n_of_tests.append(0)
+                    self.n_of_runs += 1
+                    self.n_of_tests.append(0)
                     temp = float(findall('-?\d+.\d+', line)[0])
-                    temperatures.append(temp)
+                    self.temperatures.append(temp)
                 elif 'Fliessgeschwindigkeit:' in line:
-                    press = float(findall('\d+.\d+', line)[0])
-                    pressures.append(press)
+                    vel = float(findall('\d+.\d+', line)[0])
+                    self.velocities.append(vel)
                 elif 'Messwert:' in line:
-                    energy = float(findall('\d+.\d+', line)[0])
-                    energies.append(energy)
+                    mes = float(findall('\d+.\d+', line)[0])
+                    self.measurements.append(mes)
                 elif 'Test Nr.' in line:
-                    n_of_tests[n_of_runs] += 1
+                    self.n_of_tests[self.n_of_runs] += 1
 
-        self.mean_T = mean(temperatures)
-        self.mean_v = mean(velocities)
-        self.mean_n = mean(n_of_tests)
-        self.mean_w = mean(measurements)
+        self.mean_T = mean(self.temperatures)
+        self.mean_v = mean(self.velocities)
+        self.mean_n = mean(self.n_of_tests)
+        self.mean_w = mean(self.measurements)
 
-        self.std_T = std(temperatures)
-        self.std_v = std(velocities)
-        self.std_n = std(n_of_tests)
-        self.std_w = std(measurements)
+        self.std_T = std(self.temperatures)
+        self.std_v = std(self.velocities)
+        self.std_n = std(self.n_of_tests)
+        self.std_w = std(self.measurements)
 
-        self.range_T = (min(temperatures), max(temperatures))
-        self.range_v = (min(velocities), max(velocities))
-        self.range_n = (min(n_of_tests), max(n_of_tests))
-        self.range_w = (min(measurements), max(measurements))
+        self.range_T = (min(self.temperatures), max(self.temperatures))
+        self.range_v = (min(self.velocities), max(self.velocities))
+        self.range_n = (min(self.n_of_tests), max(self.n_of_tests))
+        self.range_w = (min(self.measurements), max(self.measurements))
 
     def __str__(self):
         ret = "=== Analyse der Ergebnisse in "
@@ -72,4 +74,9 @@ class daten_analyse:
 
     def plot_results(self):
         import matplotlib.pyplot as plt
-        pass
+        # works okay
+        plt.plot(self.temperatures)
+        plt.title('test')
+        plt.grid(True)
+        plt.savefig('test.pdf')
+        plt.show
