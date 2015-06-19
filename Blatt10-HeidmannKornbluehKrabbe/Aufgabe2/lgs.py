eb8ae17be90c60e937679b0209a223a9efe6b3f7
tronje <tronjekrabbe@gmail.com>
2015-06-19 19:55:38 +0200

    outlined lgs class for 10/2

---
+++ Blatt10-HeidmannKornbluehKrabbe/Aufgabe2/lgs.py
@@ -0,0 +1,27 @@
+'''
+Heidmann
+Kornblueh
+Krabbe
+'''
+class lgs():
+    ''' Klasse zum loesen eines linearen Gleichungssystems'''
+    def __init__(self, data):
+        self.koeffmat = None
+        self.lsgvektor = None
+        pass
+
+    def istgueltig(self):
+        pass
+
+    def isthomogen(self):
+        pass
+
+    def isteindeutig(self):
+        pass
+
+    def bestimme_loesungsvektor(self):
+        pass
+
+    def __str__(self):
+        pass
+
