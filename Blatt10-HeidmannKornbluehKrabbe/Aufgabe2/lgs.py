dc9a76a9369bc79f1fb97f828dcea8ddaafdbb33
NodmGatall <oliverheidmann@hotmail.de>
2015-06-21 20:57:05 +0200

    implemented input, matrix calculation and generation of lsg vec

--- Blatt10-HeidmannKornbluehKrabbe/Aufgabe2/lgs.py
+++ Blatt10-HeidmannKornbluehKrabbe/Aufgabe2/lgs.py
@@ -1,13 +1,19 @@
-'''
-Heidmann
-Kornblueh
-Krabbe
-'''
-class lgs():
-    ''' Klasse zum loesen eines linearen Gleichungssystems'''
+# '''
+# Heidmann
+# Kornblueh
+# Krabbe
+# '''
+
+import re
+
+p = re.compile(r'[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?')
+
+class Lgs:
+    # ''' Klasse zum loesen eines linearen Gleichungssystems'''
     def __init__(self, data):
-        self.koeffmat = None
-        self.lsgvektor = None
+        self.koeffmat = data
+        self.lsgvektor = [0 for x in range(len(data))]
+
         pass
 
     def istgueltig(self):
@@ -20,8 +26,81 @@ class lgs():
         pass
 
     def bestimme_loesungsvektor(self):
+
+        l =  len(self.koeffmat)
+        for i in range(l):
+            for j in range(i+1,l):
+                faktor = -1 * self.koeffmat[j][i] / self.koeffmat[i][i]
+                for k in range(i,len(self.koeffmat[0])):
+                    print("lol")
+                    
+                    self.koeffmat[j][k] = self.koeffmat[j][k] + faktor * self.koeffmat[i][k]
+        print(self.koeffmat)
+
+        for i in range(l):
+            self.lsgvektor[i] = self.koeffmat[i][len(self.koeffmat[0]) - 1] 
+     
         pass
 
     def __str__(self):
         pass
 
+def getInput():
+    allIsDigit = False
+    done = False
+    number_of_entries_in_line = 0
+    number_of_lines = 0
+    matrix = []
+
+    x = input("please enter matrix line or type \"done\" when done: ")    
+    y = x.split()
+
+    matrix.append(y)
+
+    allIsDigit = all(p.match(elem) for elem in matrix[number_of_lines])
+
+    if allIsDigit == False:
+        print("Error: Input has non digit parts")
+        exit()
+    matrix[number_of_lines] = [float(elem) for elem in matrix[number_of_lines]]
+    number_of_lines += 1
+    number_of_entries_in_line = len(y)
+
+    while done == False:
+
+        #check if all line have the same length
+        if len(y) != number_of_entries_in_line:
+            print("Error cannot build matrix from lines with different lengths")
+            exit()
+
+        #Tell user to input a line
+        x = input("please enter matrix line or type \"done\" when done: ")
+
+        #If not done split the string and add it to Lgs
+        if x != "done":
+            y = x.split()
+            matrix.append(y)
+            allIsDigit = all(p.match(elem)  for elem in matrix[number_of_lines])
+            if allIsDigit == False:
+                print("Error: Input has non digit parts")
+                exit()
+            matrix[number_of_lines] = [float(elem) for elem in matrix[number_of_lines]]
+            number_of_lines += 1
+        #End the loop
+        else:
+            done = True
+
+    if number_of_lines != number_of_entries_in_line  - 1:
+        print("cannot generate matrix: line lenght and number of lines differ")
+        exit();
+
+    print("== Input end ==")
+
+    return matrix
+
+matrix = getInput()
+lgs = Lgs(matrix)
+print(lgs.koeffmat)
+lgs.bestimme_loesungsvektor()
+print("test")
+print(lgs.lsgvektor)
